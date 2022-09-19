b249e4d67d87c1d9a17380d842f4da10e2515892
toutou <toutounode@gmail.com>
2022-09-19 22:39:25 +0900

    方針の追加

---
+++ VRCPicSimilarity/SelectBalancedPic.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace VRCPicSimilarity;
+
+/// <summary>
+/// 最大100枚の画像から最近撮られた10枚を選び1枚(A)ランダムに取り出す
+/// ImageSetを時系列の降順にソート
+/// Aからthreshold以上のImageSetをつくる
+/// ImageSetからランダムにBを選ぶ
+/// Bからthreshold以上のImageSetを作る
+/// ImageSetからランダムにCを選ぶ
+/// Cからthreshold以上のImageSetを作る
+/// ImageSetからランダムにDを選ぶ
+/// 途中でImageSetの大きさが0になるか4毎選ぶかで終了
+/// </summary>
+public class SelectBalancedPic
+{
+    private string[] _paths;
+
+    public SelectBalancedPic(params string[] paths)
+    {
+        _paths = paths;
+    }
+}
